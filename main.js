c327fa961dd64067912270c264c180a447ff4e3b
-Jao- <112592702+joaofical@users.noreply.github.com>
2024-05-09 07:46:27 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new bate("2024-05-10T00:00:00");
 
-for (let i=0; i<botoes.length; i++){
-    botoes(i).onclick=function(){
-        for(j=0;j<botoes.length;j++){
+contadores[0].textContent = tempoObjetivo1;
+for (let i = 0; i < botoes.length; i++) {
+    botoes(i).onclick = function () {
+        for (j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
-        botoes[i].classList.add("ativo");
     }
+    botoes[i].classList.add("ativo");
+    textos[i].classList.add("ativo");
 }
