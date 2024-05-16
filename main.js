72bd3ff04756a0d652afbaeffaf33ada02e20770
-Jao- <112592702+joaofical@users.noreply.github.com>
2024-05-16 07:33:28 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new bate("2024-05-10T00:00:00");
 
-contadores[0].textContent = tempoObjetivo1;
 for (let i = 0; i < botoes.length; i++) {
-    botoes(i).onclick = function () {
+    botoes[i].onclick = function () {
         for (j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
-    botoes[i].classList.add("ativo");
-    textos[i].classList.add("ativo");
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-05-20T00:00:00");
+
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos%=60;
+    minutos%=60;
+    horas%=24;
+
+    return dias + "dias " + horas + "horas " + minutos + "minutos " + segundos + "segundos ";
 }
