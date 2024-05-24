449ea01966e2bdd6e4324555e1a6cc0d38e12655
-Jao- <112592702+joaofical@users.noreply.github.com>
2024-05-24 09:22:06 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -32,13 +32,23 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
 
-    return dias + "dias " + horas + "horas " + minutos + "minutos " + segundos + "segundos ";
+    return dias + "dias " + horas + "horas " + minutos + "minutos " + segundos + "segundos";
 }
 
-function atualizadorCronometro() {
+function atualizaCronometro() {
     document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
     document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
     document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
     document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
 
+    for (let i = 0; i < contadores.length; i++) {
+        //contadores[1].textContent = calculaTempo(tempos[1]);
+    }
 }
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+
+comecaCronometro();
